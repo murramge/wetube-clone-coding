31adb993acc7321b6cd0e1f85cef5aea9eaa8de9
daram <dmdgi37@naver.com>
2022-01-11 14:10:41 +0900

    wetube-clone-server-request-response

--- src/server.js
+++ src/server.js
@@ -4,6 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+    return res.send("I still love you");
+
+}
+
+const handleLogin = (req, res) => {
+    return res.send("Login here.");
+}
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`)
 
 app.listen(PORT, handleListening)
