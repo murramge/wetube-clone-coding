390e8f650490d7e09b8b2f4cfbae7ea41c8f20a5
daram <dmdgi37@naver.com>
2022-01-12 22:40:42 +0900

    wetube-clone-router

---
+++ README.md
@@ -0,0 +1,22 @@
+router 설정
+
+
+(global router)
+
+/ → 이것은 Homepage가 될 것이다.
+/join → 회원가입할 때 Join이다.
+/login → 로그인을 하기 위한 login 페이지이다.
+/Search → 검색 페이지인 Search 이다.
+
+(user router)
+
+/users/edit → 유저가 프로필을 수정하는 페이지이다.
+/users/delete → 유저가 프로필을 삭제하는 페이지이다.
+
+(video router)
+
+/videos/watch → 동영상 시청 페이지이다. 
+/videos/edit → 동영상 수정 페이지이다. 
+/videos/delete → 동영상 삭제 페이지이다.
+/videos/comment → 동영상 댓글 페이지이다 
+/videos/comments/delete → 동영상 댓글 삭제 페이지이다.
--- src/server.js
+++ src/server.js
@@ -1,31 +1,38 @@
 import express from "express";
-
+import morgan from "morgan";
 const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-    console.log(`${req.method}: ${req.url}`);
-    next();
-}
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1> Not Allowed <h1>")
-    }
-    console.log("allowed, you may continue");
-    next();
-}
-const handleProtected = (req, res) => {
-    return res.send("welcome to the private lounge.");
-}
-const handleHome = (req, res) => {
-    return res.send("i love middleware");
-}
+const logger = morgan("dev");
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/",  handleHome);
-app.get("/protected", handleProtected);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("EditUser");
+
+userRouter.get("/edit", handleEditUser);
+
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("WatchVideo");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+
+app.use("/",globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`)
 
