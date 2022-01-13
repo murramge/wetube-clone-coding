f7a25eb6e24c58f323da0de7eb1b4bbee9f500ed
daram <dmdgi37@naver.com>
2022-01-13 14:19:24 +0900

    wetube-clone-clean-coding-controller

---
+++ src/controllers/videoController.js
@@ -0,0 +1,3 @@
+export const trending = (req, res) => res.send("Home Page Videos");
+export const watch = (req, res) => res.send("Video watch");
+export const edit = (req, res) => res.send("Video edit");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,8 @@
 import express from "express";
+import {watch,edit} from "../controllers/videoController"
 const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("WatchVideo");
-videoRouter.get("/watch", handleWatchVideo);
+
+videoRouter.get("/watch", watch);
+videoRouter.get("/edit",edit);
+
 export default videoRouter;
