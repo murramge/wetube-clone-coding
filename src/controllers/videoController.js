a27a0069d2ea8a97eb41002dfb65c251daf16c55
daram <dmdgi37@naver.com>
2022-01-17 22:20:53 +0900

    wetube-clone-mixins-and-various-pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,34 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+
+
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments:2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id : 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments:2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id : 1,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments:2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id : 1,
+        },
+    ];
+    return res.render("home", {pageTitle: "Home",videos})
+    };
 export const upload = (req, res) => res.send("upload video");
 export const see = (req, res) => {
     return res.render("watch");};
