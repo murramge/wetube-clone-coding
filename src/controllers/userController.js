623c030b89f7a487630653162a8c8f56383a9966
daram <dmdgi37@naver.com>
2022-01-28 03:53:49 +0900

    wetube-user-basesetting-with-passwordhash

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,21 @@
-export const join = (req, res) => res.send("join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", {
+    pageTitle: "Join"
+});
+export const postJoin = async (req, res) => {
+    const {name,username,email,password,location} = req.body;
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
+    });
+    res.redirect("/login");
+};
 export const edit = (req, res) => res.send("user edit");
 export const remove = (req, res) => res.send("user remove");
 export const login = (req, res) => res.send("login");
-export const logout = (req,res) => res.send("logout");
+export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("see user");
--- src/init.js
+++ src/init.js
@@ -1,5 +1,6 @@
 import "./db";
 import "./models/Videos";
+import "./models/User";
 import app from "./server";
 
 const PORT = 4000;
