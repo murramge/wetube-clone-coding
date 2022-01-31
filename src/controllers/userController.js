4042df1574f31827fd76bac7cf00223f0cef8e79
daram <dmdgi37@naver.com>
2022-01-31 20:04:37 +0900

    wetube-clone-user-login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", {
     pageTitle: "Join"
@@ -7,18 +8,18 @@ export const postJoin = async (req, res) => {
     const {name,username,email,password,password2,location} = req.body;
     const exists = await User.exists({$or: [{username}, {email}] });
     if(password !== password2) {
-        return res.render("join", {
+        return res.status(400).render("join", {
             pageTitle: "Join",
             errorMessage: "password confirmation does not match.",
         });
     }
     if(exists) {
-        return res.render("join", {
+        return res.status(400).render("join", {
             pageTitle: "Join",
             errorMessage: "This username/email is alread taken",
         });
     }
-   
+    try{
     await User.create({
         name,
         username,
@@ -26,10 +27,41 @@ export const postJoin = async (req, res) => {
         password,
         location,
     });
-    res.redirect("/login");
+    return res.redirect("/login");
+    }
+    catch(error){
+        return res.status(400).render("join", {
+            pageTitle: "upload Video",
+            errorMessage: error._message,
+        })
+    }
 };
+
+export const getLogin = (req, res) => {
+    return res.render("login", {pageTitle:"Login"});
+}
+export const postLogin = async(req, res) => {
+    //계정이 존재하는지, 비밀번호가 정확한지 
+    const {username,password} = req.body;
+    const pageTitle = "Login";
+    const user = await User.findOne({username});
+    if(!user) {
+        return res.status(400).render("login", {pageTitle, errorMessage:"An account with this username/password does not exists"})
+    }
+    const ok = await bcrypt.compare(password, user.password);
+    if(!ok) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "wrong password",
+        });
+    }
+    console.log("Log user in! comming soon")
+    return res.redirect("/");
+}
+
+
 export const edit = (req, res) => res.send("user edit");
 export const remove = (req, res) => res.send("user remove");
-export const login = (req, res) => res.send("login");
+
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("see user");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -20,7 +20,7 @@ export const watch = async(req, res) => {
     } = req.params;
     const video = await Video.findById(id);
    if(!video){
-    return res.render("404", {pageTitle:"video not found."});
+    return res.status(404).render("404", {pageTitle:"video not found."});
     }
     return res.render("watch", {
         pageTitle: video.title, video
@@ -32,7 +32,7 @@ export const getEdit = async(req, res) => {
     } = req.params;
     const video = await Video.findById(id);
    if(video === null){
-    return res.render("404", {pageTitle:"video not found."});
+    return res.status(404).render("404", {pageTitle:"video not found."});
     }
     return res.render("edit", {
         pageTitle: `Edit ${video.title}`, video
@@ -45,7 +45,7 @@ export const postEdit = async (req, res) => {
     const {title, description, hashtags} = req.body;
     const video = await Video.exists({_id: id});
     if(video === null){
-        return res.render("404", {pageTitle:"video not found."});
+        return res.status(404).render("404", {pageTitle:"video not found."});
         }
         await Video.findByIdAndUpdate(id, {
             title, description, hashtags: Video.formatHashtags(hashtags),
@@ -90,7 +90,7 @@ export const deleteVideo = async(req, res)=> {
             title: {
                 $regex: new RegExp(keyword, "i")
             }
-         })
-     }
-     return res.render("search", {pageTitle: "Search",videos});
- }
+        })
+    }
+    return res.render("search", {pageTitle: "Search",videos});
+}
