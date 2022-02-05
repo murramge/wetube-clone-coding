ac500ce19b338bd602958fcfa984fa817ead6f96
daram <dmdgi37@naver.com>
2022-02-06 00:17:56 +0900

    wetube-clone-video-upload

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,12 +2,12 @@ import express from "express";
 import {getEdit,postEdit,logout,see,startGithublogin,finishGithublogin, getChangePassword, postChangePassword} from "../controllers/userController"
 import { publicOnlyMiddleware } from "../middlewares";
 import { protectorMiddleware } from "../middlewares";
-import {uploadFiles} from "../middlewares";
+import {avatarUpload} from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout",protectorMiddleware,logout);
-userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(uploadFiles.single("avatar"), postEdit);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(avatarUpload.single("avatar"), postEdit);
 userRouter.get("/github/start",publicOnlyMiddleware, startGithublogin);
 userRouter.get("/github/finish",publicOnlyMiddleware,finishGithublogin);
 userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
