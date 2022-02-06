40c58edf1ea70d482c8ed200f00fecdf6aa87761
daram <dmdgi37@naver.com>
2022-02-07 02:37:29 +0900

    wetube-clone-video-user-model-connection

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -244,12 +244,11 @@ export const logout = (req, res) => {
 };
 export const see = async(req, res) => {
     const {id} = req.params;
-    const user = await User.findById(id);
+    const user = await User.findById(id).populate("videos");
 
     if(!user){
         return res.status(404).render("404", {pageTitle:"User not found"});
     }
-    const videos = await Video.find({owner:user.id});
-    return res.render("users/profile",{pageTitle:`${user.name}의 Profile`, user,videos,})
+    return res.render("users/profile",{pageTitle:`${user.name}의 Profile`, user})
 };
 
