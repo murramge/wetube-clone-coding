25f63b8ed3be7fae7b6e5f22d6dad58fe1cc2daa
daram <dmdgi37@naver.com>
2022-04-19 01:30:28 +0900

    s3 uploads

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,20 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  }
+})
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "hamstertube",
+  acl: "public-read",
+})
+
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
@@ -28,11 +44,13 @@ export const publicOnlyMiddleware = (req, res, next) => {
    limits: {
      fileSize: 3000000,
    },
+   storage: multerUploader,
  });
  export const videoUpload = multer({
    dest: "uploads/videos/",
    limits: {
      fileSize: 10000000,
    },
+    storage: multerUploader,
  });
  
